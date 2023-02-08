e5d714916203c04c715a232fda622dba96375704
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-02-08 20:35:55 +0900

    Create variable3.js

---
+++ variable3.js
@@ -0,0 +1,9 @@
+// 상수가 필요한 이유
+
+const currentYear = 2022; // 현재 연도
+
+let birthYear = parseInt(prompt("태어난 해를 입력하세요")); //  태어난 연도
+
+let age = currentYear - birthYear + 1; // 나이
+
+console.log(age); // 콘솔에 출력
